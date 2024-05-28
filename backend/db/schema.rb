8420154a1f4b8dc7795388cf766ea76010ea421f
jasdeep-dev <jasdeepg21@gmail.com>
2024-05-28 18:05:32 -0400

    updated schema.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,34 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_526_212_431) do
-  create_table 'inventories', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.bigint 'store_id', null: false
-    t.bigint 'shoe_model_id', null: false
-    t.integer 'inventory', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['shoe_model_id'], name: 'index_inventories_on_shoe_model_id'
-    t.index %w[store_id shoe_model_id], name: 'index_inventories_on_store_id_and_shoe_model_id', unique: true
-    t.index ['store_id'], name: 'index_inventories_on_store_id'
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_212431) do
+  create_table "inventories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "store_id", null: false
+    t.bigint "shoe_model_id", null: false
+    t.integer "inventory", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shoe_model_id"], name: "index_inventories_on_shoe_model_id"
+    t.index ["store_id", "shoe_model_id"], name: "index_inventories_on_store_id_and_shoe_model_id", unique: true
+    t.index ["store_id"], name: "index_inventories_on_store_id"
   end
 
-  create_table 'shoe_models', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'name', null: false
-    t.decimal 'price', precision: 10, scale: 2, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_shoe_models_on_name', unique: true
+  create_table "shoe_models", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_shoe_models_on_name", unique: true
   end
 
-  create_table 'stores', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'address'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['name'], name: 'index_stores_on_name', unique: true
+  create_table "stores", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_stores_on_name", unique: true
   end
 
-  add_foreign_key 'inventories', 'shoe_models'
-  add_foreign_key 'inventories', 'stores'
+  add_foreign_key "inventories", "shoe_models"
+  add_foreign_key "inventories", "stores"
 end
